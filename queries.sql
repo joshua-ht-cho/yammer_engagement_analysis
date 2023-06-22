0c021bb22ff4db77eef391062dee0600fd20e9f5
Joshua Cho <134950540+joshua-ht-cho@users.noreply.github.com>
2023-06-22 19:50:49 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,4 +1,24 @@
--- Yammer User Engagement SQL Analysis
+-- Yammer User Engagement SQL Analysis (BigQuery)
 
--- 1. Investigate tables using SELECT *
--- 2. 
+-- Step 1: Investigate tables using SELECT *
+-- Step 2: Find engagement trend by examining weekly active users from May through August 2022
+
+
+-- Step 1: Investigate tables using SELECT *
+select * from yammer.users limit 100;
+select * from yammer.events limit 100;
+select * from yammer.emails limit 100;
+
+
+-- Step 2: Find engagement trend by examining weekly active users from May through August 2022
+-- 'Weekly active users' metric is defined as 'accounts that logged in during the week starting on the measurement date.
+
+select
+    date_trunc(CAST(occurred_at as timestamp), week) AS week,
+    count(distinct user_id) as weekly_active_users
+from yammer.events
+where 
+    event_type = 'engagement'
+    and event_name = 'login'
+group by 1
+order by 1
